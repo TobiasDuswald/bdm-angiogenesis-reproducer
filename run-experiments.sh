15018e7270f5dac8de39b0a2306af7a642465db9
Tobias Duswald <tobias.duswald@cern.ch>
2023-06-08 10:34:56 +0200

    Add sourcing of util/main to experiment run

--- run-experiments.sh
+++ run-experiments.sh
@@ -4,6 +4,10 @@
 # run the large-scale simulation because it is expected to run for several
 # days. The lagre-scale simulation can be run by executing a different script.
 
+# Source the util/main.sh script for ParaView if hostename is "docker-container"
+if [ "$HOSTNAME" = "docker-container" ]; then
+    . util/main.sh
+fi
 
 # Run the experiments for the paper
 ./experiments/initial-state/run.sh
