3fd7cc5798fbc8c131020c9ea38e2b5267806205
Tobias Duswald <tobias.duswald@cern.ch>
2023-06-08 11:52:16 +0200

    Update LB's Zenodo package

--- util/machine-specific/docker-container/prerequisites.sh
+++ util/machine-specific/docker-container/prerequisites.sh
@@ -1,7 +1,7 @@
 # Statement:
 # The docker infrastructure used here was developed by Lukas Breitwieser for
 # his published research work. 
-# It is available at https://zenodo.org/record/5121618
+# It is available at https://doi.org/10.5281/zenodo.6463816
 # Here we simply reuse it with minor modifications.
 #
 # File Author: Lukas Breitwieser
